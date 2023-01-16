952ec16b4422ee9d4cd6b1cdbcdb141bbc319dd5
Steven Crosby <w0218814@nscc.ca>
2023-01-16 12:08:06 -0400

    Commit changes

---
+++ PROG2200-700/Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("What city do you live in?");
+            var city = Console.ReadLine();
+
+            Console.WriteLine("What is your current career?");
+            var career = Console.ReadLine();
+
+            Console.WriteLine("Your name is: {0}", name);
+            Console.WriteLine("Your age is: {0}", age);
+            Console.WriteLine("Your birth month is: {0}", month);
+            Console.WriteLine("Your city is: {0}", city);
+            Console.WriteLine("Your career is: {0}", career);
+
+        }
+    }
+}
