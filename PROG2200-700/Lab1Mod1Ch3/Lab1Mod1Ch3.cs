051fa8293d85eda8cafbedf4f0377702d5681d50
Steven Crosby <w0218814@nscc.ca>
2023-01-16 13:07:58 -0400

    Lab1Mod1Ch3 done

--- PROG2200-700/Lab1Mod1Ch3/Lab1Mod1Ch3.cs
+++ PROG2200-700/Lab1Mod1Ch3/Lab1Mod1Ch3.cs
@@ -11,12 +11,15 @@ namespace Lab1Mod1Ch3
                 for (int i = 1; i <= 10; i++)
                 {
                     Console.WriteLine(i);
-                }
-                for (int i = 10; i >= 1; i--)
-                {
-                    Console.WriteLine(i);
+
+                    int j = 10; 
+                    if (j >= 1 && i > 10)
+           
+                        Console.WriteLine(j);
+                    j--;
+                    }
+
                 }
             }
         }
     }
-}
